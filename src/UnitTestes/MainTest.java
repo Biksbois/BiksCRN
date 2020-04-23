38c4adee0122af174f85ad77200f91a39d4c5130
Jeppe <jeppe@bandsholm.com>
2020-04-23 12:20:49 +0200

    UnitTests on the way

---
+++ src/UnitTestes/MainTest.java
@@ -0,0 +1,14 @@
+package UnitTestes;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        Assert.assertEquals(1,1);
+    }
+}
