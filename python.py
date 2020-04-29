7e16e2b6944b506eaaff502f70169d5ea4b72714
Jeppe <jeppe@bandsholm.com>
2020-04-29 14:29:27 +0200

    Improvements to ploting, and the calculations when many species exisit

--- python.py
+++ python.py
@@ -50,98 +50,16 @@ def mix(sampleList):
 def equilibrate(sample, stepsize, times):
     sample.h = stepsize
     sample.steps = times
-    ani = FuncAnimation(plt.gcf(), sample.Animate, interval=2)
+    ani = FuncAnimation(plt.gcf(), sample.Animate, interval=50)
     plt.show()
 
 class SampleQ():
     sample = {
-        "A":[10],
-        "U":[0]
+        "A":[0],
+        "X":[10]
     }
 
-    temp = [
-        10
-    ]
-
-    temp2 = [
-        0
-    ]
-
-    temp3 = [
-        10
-    ]
-
-    temp5 = [
-        0
-    ]
-
-    def TitAccumilationA(self, act, titra):
-        titra += self.h
-        if(act <= titra):
-            result = math.floor(titra/act)
-            titra = titra - act * result
-            return result, titra
-        else:
-            return 0, titra
-
-    stepList = []
-    index = count()
-    steps = 100
-    h = 0.0025
-
-    def Euler(self, i) :
-        if(i < self.steps):
-            # r1=1*(self.sample.get("A")[-1]**2)*(self.sample.get("O")[-1])
-            
-            # #O+2A -> U +2*O
-            # #self.temp3.append(self.temp[-1])
-            # #self.temp5.append(self.temp2[-1])
-
-            # self.temp.append(self.temp[-1]+(r1)*self.h)
-            # self.temp2.append(self.temp2[-1]+(r2)*self.h)
-
-            # self.sample["A"].append(self.temp[-1]*-2)
-            # self.sample["U"].append(self.temp2[-1]*1)
-            # self.sample["O"].append(self.temp2[-1]*-1)
-
-            lhs1=1*(self.sample.get("A")[-1]**3)
-            
-
-            #O+2A -> U +2*O
-            #self.temp3.append(self.temp[-1])
-            #self.temp5.append(self.temp2[-1])
-
-            #self.temp.append(self.temp[-1]+(r1)*self.h)
-            #self.temp2.append(self.temp2[-1]+(r1)*self.h)
-
-            self.sample["A"].append(self.sample.get("A")[-1]+lhs1*-3*self.h)
-            self.sample["U"].append(self.sample.get("U")[-1]+lhs1*1*self.h)
-
-
-    @staticmethod
-    def Animate(i) :
-        plt.cla()
-
-        if(i <= SampleQ.steps):
-            SampleQ.stepList.append(next(SampleQ.index)*SampleQ.h)
-
-        for key, value in SampleQ.sample.items():
-            plt.plot(SampleQ.stepList, value,'bo', label=key +"  = " +str(SampleQ.sample.get(key)[-1]))
-            plt.legend()
-
-        if(i <= SampleQ.steps):
-            SampleQ.Euler(SampleQ, i)
-
-class SampleL():
-    sample = {
-        "A":[100],
-        "C":[80],
-        "D":[70],
-        "U":[1],
-        "E":[60]
-    }
-
-    def TitAccumilationA(self, act, titra):
+    def AccTitration(self, act, titra):
         titra += self.h
         if(act <= titra):
             result = math.floor(titra/act)
@@ -156,130 +74,38 @@ class SampleL():
     h = 0.0025
 
     AddMol0=0
-    AddMol1=0
-    AddMol2=0
-    AddMol3=0
-    AddMol4=0
-
-    RemMol0=0
-    RemMol1=0
-    RemMol2=0
-    RemMol3=0
-    RemMol4=0
 
     def Euler(self, i) :
         if(i < self.steps):
-            r1=1*self.sample.get("A")[-1]
-            r1=1*self.sample.get("A")[-1]*self.sample.get("C")[-1]
-            r1=2*self.sample.get("D")[-1]
-            r1=2*self.sample.get("D")[-1]
-            r1=1*self.sample.get("U")[-1]*self.sample.get("E")[-1]
-            r1=1*self.sample.get("U")[-1]*self.sample.get("E")[-1]
-            r1=3*self.sample.get("A")[-1]*self.sample.get("C")[-1]
-            r1=3*self.sample.get("E")[-1]
+            r0=1*(self.sample.get("A")[-1]**1)
+            r1=2*(self.sample.get("X")[-1]**3)
 
-            self.sample["A"].append((-r1-r1+r1+r1-r1)*self.h+self.sample.get("A")[-1])
-            self.sample["C"].append((-r1+r1+r1-r1+r1)*self.h+self.sample.get("C")[-1])
-            self.sample["D"].append((r1-r1-r1+r1)*self.h+self.sample.get("D")[-1])
-            self.sample["U"].append((r1+r1-r1-r1+r1)*self.h+self.sample.get("U")[-1])
-            self.sample["E"].append((r1-r1-r1+r1-r1)*self.h+self.sample.get("E")[-1])
+            self.sample["A"].append(self.sample.get("A")[-1]+(r0*(-1)+r1*(1))*self.h)
+            self.sample["X"].append(self.sample.get("X")[-1]+(r0*(1)+r1*(-3))*self.h)
 
     def ApplyTitration(self,i):
-        Result, self.AddMol0 = self.TitAccumilationA(self, 1, self.AddMol0)
-        self.sample["U"][-1] = self.sample.get("U")[-1]+Result*1
-        Result, self.AddMol1 = self.TitAccumilationA(self, 1, self.AddMol1)
-        self.sample["U"][-1] = self.sample.get("U")[-1]+Result*1
-        Result, self.AddMol2 = self.TitAccumilationA(self, 1, self.AddMol2)
-        self.sample["U"][-1] = self.sample.get("U")[-1]+Result*1
-        Result, self.AddMol3 = self.TitAccumilationA(self, 1, self.AddMol3)
-        self.sample["U"][-1] = self.sample.get("U")[-1]+Result*1
-        Result, self.AddMol4 = self.TitAccumilationA(self, 1, self.AddMol4)
-        self.sample["U"][-1] = self.sample.get("U")[-1]+Result*1
-        if self.sample.get("U")[-1]-1 <= 0:
-            self.sample.get("U")[-1] = 0
-        else:
-            Result, self.RemMol0 = self.TitAccumilationA(self, 1, self.RemMol0)
-            self.sample["U"][-1] = self.sample.get("U")[-1]-Result*1
-        if self.sample.get("U")[-1]-1 <= 0:
-            self.sample.get("U")[-1] = 0
-        else:
-            Result, self.RemMol1 = self.TitAccumilationA(self, 1, self.RemMol1)
-            self.sample["U"][-1] = self.sample.get("U")[-1]-Result*1
-        if self.sample.get("U")[-1]+1/self.sample.get("U")[-1]>9:
-            if self.sample.get("U")[-1]-1 <= 0:
-                self.sample.get("U")[-1] = 0
-            else:
-                Result, self.RemMol2 = self.TitAccumilationA(self, 1, self.RemMol2)
-                self.sample["U"][-1] = self.sample.get("U")[-1]-Result*1
-        if self.sample.get("U")[-1]-1 <= 0:
-            self.sample.get("U")[-1] = 0
-        else:
-            Result, self.RemMol3 = self.TitAccumilationA(self, 1, self.RemMol3)
-            self.sample["U"][-1] = self.sample.get("U")[-1]-Result*1
-        if self.sample.get("U")[-1]<6:
-            if self.sample.get("U")[-1]-1 <= 0:
-                self.sample.get("U")[-1] = 0
-            else:
-                Result, self.RemMol4 = self.TitAccumilationA(self, 1, self.RemMol4)
-                self.sample["U"][-1] = self.sample.get("U")[-1]-Result*1
+        if self.sample.get("X")[-1]<2:
+            Result, self.AddMol0 = self.AccTitration(self, 0.1, self.AddMol0)
+            self.sample["X"][-1] = self.sample.get("X")[-1]+Result*1
 
 
     @staticmethod
     def Animate(i) :
         plt.cla()
 
-        if(i <= SampleL.steps):
-            SampleL.stepList.append(next(SampleL.index)*SampleL.h)
+        if(i <= SampleQ.steps):
+            SampleQ.stepList.append(next(SampleQ.index)*SampleQ.h)
 
-        for key, value in SampleL.sample.items():
-            plt.plot(SampleL.stepList, value,'b:', label=key)
-            plt.legend()
+        for key, value in SampleQ.sample.items():
+            plt.plot(SampleQ.stepList, value, label=key + " = {:.2f} ".format(SampleQ.sample.get(key)[-1]))
 
-        if(i <= SampleL.steps):
-            SampleL.Euler(SampleL, i)
-            SampleL.ApplyTitration(SampleL, i+1)
+        plt.xlabel('Time (t)')
+        plt.ylabel('Concentration (mol/L)')
+        plt.legend()
 
-
-class Sampleempty():
-    sample = {
-    }
-
-    def TitAccumilationA(self, act, titra):
-        titra += self.h
-        if(act <= titra):
-            result = math.floor(titra/act)
-            titra = titra - act * result
-            return result, titra
-        else:
-            return 0, titra
-
-    stepList = []
-    index = count()
-    steps = 100
-    h = 0.0025
-
-    def Euler(self, i) :
-        pass
-
-    @staticmethod
-    def Animate(i) :
-        plt.cla()
-
-        if(i <= Sampleempty.steps):
-            Sampleempty.stepList.append(next(Sampleempty.index)*Sampleempty.h)
-
-        for key, value in Sampleempty.sample.items():
-            plt.plot(Sampleempty.stepList, value,'bo', label=key)
-            plt.legend()
-
-        if(i <= Sampleempty.steps):
-            Sampleempty.Euler(Sampleempty, i)
+        if(i <= SampleQ.steps):
+            SampleQ.Euler(SampleQ, i)
+            SampleQ.ApplyTitration(SampleQ, i+1)
 
 
-
-sample = {
-    "II":[1],
-    "A":[100],
-    "B":[12]
-}
-equilibrate(SampleQ, 5.0E-4, 1000)
+equilibrate(SampleQ, 5.0E-4, 20000)
--- src/simpleAdder/interpret/Objects/CodeGenerationOBJ/Euler.java
+++ src/simpleAdder/interpret/Objects/CodeGenerationOBJ/Euler.java
@@ -1,11 +1,13 @@
 package simpleAdder.interpret.Objects.CodeGenerationOBJ;
 
+import com.company.node.Switch;
 import javafx.util.Pair;
 import simpleAdder.interpret.Objects.SymolTableOBJ.reaction;
 import simpleAdder.interpret.Objects.SymolTableOBJ.SymbolTableType;
 import simpleAdder.interpret.TypeCheckers.BetaStackToString;
 import simpleAdder.interpret.GetMethods.ViableVariable;
 
+import javax.print.DocFlavor;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -78,13 +80,13 @@ public class Euler extends CodeGenerationMethods {
     private String GenerateEuler(List<reaction> reacs, int Level, String str){
         String PrettyResult = ApplyTap(Level, GenerateEulerDcl(str));
         HashMap<String, String> species = new HashMap<>();
-
         int rNum = 0;
         Level++;
         PrettyResult += ApplyTap(Level,"if(i < self.steps):\n");
         Level++;
         for (reaction r : reacs){
-
+            Refragtor(r.lhsPair);
+            Refragtor(r.rhsPair);
             SetRate(r);
             rNum = NumberOfReactions(r, rNum,species);
             PrettyResult += ApplyTap(Level, r.lhsDerivedEq.getKey()+"="+r.lhsDerivedEq.getValue()+"\n");
@@ -96,13 +98,33 @@ public class Euler extends CodeGenerationMethods {
         PrettyResult += "\n";
         Level++;
         for (Map.Entry<String, String> s : species.entrySet()){
-            PrettyResult += ApplyTap(Level, "self.sample[\"" + s.getKey() + "\"].append(self.sample.get(\""+s.getKey()+"\")[-1]+("+GenerateReactionRef(s.getKey(),reacs)+")*SOMETHING*self.h\n");
+            PrettyResult += ApplyTap(Level, "self.sample[\"" + s.getKey() + "\"].append(self.sample.get(\""+s.getKey()+"\")[-1]+("+GenerateReactionRef(s.getKey(),reacs)+")*self.h)\n");
             //PrettyResult += ApplyTap(Level, "self.sample[\"" + s.getKey() + "\"].append((" + GenerateReactionRef(s.getKey(),reacs)+ ")*self.h+self.sample.get(\"" + s.getKey() + "\")[-1])\n") ;
         }
         Level--;
         Level--;
 
         return PrettyResult + "\n";
+    }
+
+    public void Refragtor(List<Pair<String,String>> Side)
+    {
+        for (int i = 0; i < Side.size(); i++)
+        {
+            for (int j = i+1; j < Side.size(); j++)
+            {
+                if(Side.get(i).getKey().equals(Side.get(j).getKey()))
+                {
+                    String Value = String.valueOf(Integer.valueOf(Side.get(i).getValue()) + Integer.valueOf(Side.get(j).getValue()));
+                    Pair<String, String> result = new Pair<>(Side.get(i).getKey(),Value);
+                    Side.remove(j);
+                    Side.remove(i);
+                    Side.add(i,result);
+                    i--;
+                   //Side.get(i).getValue() = String.valueOf(Integer.valueOf(Side.get(i).getValue()) + Integer.valueOf(Side.get(j).getValue()));
+                }
+            }
+        }
     }
 
     /***
@@ -126,7 +148,7 @@ public class Euler extends CodeGenerationMethods {
         {
             AddSpecieHash(Species, reac);
         }
-        return rNum;
+        return ++rNum;
     }
 
     private void AddSpecieHash(HashMap<String,String> Species, reaction reac)
@@ -140,7 +162,7 @@ public class Euler extends CodeGenerationMethods {
 
     private Pair<String,String> GenerateDerivedEQ(int rNum,String rate, List<Pair<String,String>> reac, HashMap<String,String> Species)
     {
-        String name = "r"+ ++rNum;
+        String name = "r"+ rNum;
         String Value = rate;
         for (Pair<String, String> p : reac) {
             if (!Species.containsKey(p.getKey())){
@@ -162,13 +184,14 @@ public class Euler extends CodeGenerationMethods {
         String PrettyResult = "";
         for (reaction r : reacList)
         {
-            PrettyResult += DerivedForOne(r.lhsPair, r.lhsDerivedEq.getKey(), specie, "-");
-            PrettyResult += DerivedForOne(r.rhsPair, r.lhsDerivedEq.getKey(), specie, "+");
+            HashMap<String,Integer> result = CountSpecies(r);
+            PrettyResult += DerivedForOne(r.lhsPair, r.lhsDerivedEq.getKey(), specie, "-",result);
+            PrettyResult += DerivedForOne(r.rhsPair, r.lhsDerivedEq.getKey(), specie, "+",result);
 
             if(!r.isOneway)
             {
-                PrettyResult += DerivedForOne(r.lhsPair, r.rhsDerivedEq.getKey(), specie, "+");
-                PrettyResult += DerivedForOne(r.rhsPair, r.rhsDerivedEq.getKey(), specie, "-");
+                PrettyResult += DerivedForOne(r.lhsPair, r.rhsDerivedEq.getKey(), specie, "+",result);
+                PrettyResult += DerivedForOne(r.rhsPair, r.rhsDerivedEq.getKey(), specie, "-",result);
             }
 
         }
@@ -178,6 +201,37 @@ public class Euler extends CodeGenerationMethods {
         return PrettyResult;
     }
 
+    public HashMap<String,Integer> CountSpecies(reaction reac)
+    {
+        HashMap<String,Integer> result = new HashMap<>();
+        CountSide(result,reac.rhsPair,vv.plus);
+        CountSide(result,reac.lhsPair,vv.minus);
+
+        return result;
+    }
+
+    public void CountSide(HashMap<String,Integer> hash, List<Pair<String, String>> side, String Symbol)
+    {
+        for (Pair<String, String> rhs: side) {
+            if(hash.containsKey(rhs.getKey()))
+            {
+                hash.replace(rhs.getKey(), hash.get(rhs.getKey()+ ApplySymbol(rhs.getValue(),Symbol)));
+            }
+            else
+            {
+                hash.put(rhs.getKey(), ApplySymbol(rhs.getValue(),Symbol));
+            }
+        }
+    }
+    public int ApplySymbol(String str, String symbol)
+    {
+        int i = Integer.valueOf(str);
+        switch(symbol)
+        {
+            case "-": return i*-1;
+            default: return i;
+        }
+    }
     /***
      * Takes a symbol and adds it to the derived equation. It will do that for each side of the reaction
      * and then finally return it to the GenerateReactionRef method.
@@ -187,12 +241,12 @@ public class Euler extends CodeGenerationMethods {
      * @Param symbol
      * @return
      */
-    private String DerivedForOne(List<Pair<String, String>> pairs, String derivedEq, String specie, String symbol){
+    private String DerivedForOne(List<Pair<String, String>> pairs, String derivedEq, String specie, String symbol, HashMap<String,Integer> species){
         String PrettyResult = "";
         for (Pair<String, String> p: pairs) {
             if(specie.equals(p.getKey()))
             {
-                PrettyResult += symbol + derivedEq;
+                PrettyResult += "+"+ derivedEq +"*("+species.get(specie) +")";
             }
         }
         return PrettyResult;
