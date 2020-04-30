806a63ed984de1f02efa87bd2788c112cf815379
KuskIV <mkusk18@student.aau.dk>
2020-04-30 13:15:41 +0200

    example

--- python.py
+++ python.py
@@ -50,13 +50,21 @@ def mix(sampleList):
 def equilibrate(sample, stepsize, times):
     sample.h = stepsize
     sample.steps = times
+    plt.figure(figsize=(12, 7),dpi=80, num='BiksCRN')
     ani = FuncAnimation(plt.gcf(), sample.Animate, interval=50)
     plt.show()
 
-class SampleQ():
+def GetPercent(i, steps):
+    result = (i/steps)*100
+    if result > 100:
+        return "100"
+    else:
+        return "{:.2f}".format(result)
+
+class SampleA():
     sample = {
-        "A":[0],
-        "X":[10]
+        "X":[0],
+        "Y":[100]
     }
 
     def AccTitration(self, act, titra):
@@ -77,14 +85,14 @@ class SampleQ():
 
     def Euler(self, i) :
         if(i < self.steps):
-            r0=1*(self.sample.get("A")[-1]**1)
-            r1=2*(self.sample.get("X")[-1]**3)
+            r0=1*(self.sample.get("Y")[-1]**1)
+            r1=2*(self.sample.get("X")[-1]**2)
 
-            self.sample["A"].append(self.sample.get("A")[-1]+(r0*(-1)+r1*(1))*self.h)
-            self.sample["X"].append(self.sample.get("X")[-1]+(r0*(1)+r1*(-3))*self.h)
+            self.sample["X"].append(self.sample.get("X")[-1]+(r0*(1)+r1*(-2))*self.h)
+            self.sample["Y"].append(self.sample.get("Y")[-1]+(r0*(-1)+r1*(1))*self.h)
 
     def ApplyTitration(self,i):
-        if self.sample.get("X")[-1]<2:
+        if self.sample.get("X")[-1]<3:
             Result, self.AddMol0 = self.AccTitration(self, 0.1, self.AddMol0)
             self.sample["X"][-1] = self.sample.get("X")[-1]+Result*1
 
@@ -93,19 +101,149 @@ class SampleQ():
     def Animate(i) :
         plt.cla()
 
-        if(i <= SampleQ.steps):
-            SampleQ.stepList.append(next(SampleQ.index)*SampleQ.h)
+        if(i <= SampleA.steps):
+            SampleA.stepList.append(next(SampleA.index)*SampleA.h)
 
-        for key, value in SampleQ.sample.items():
-            plt.plot(SampleQ.stepList, value, label=key + " = {:.2f} ".format(SampleQ.sample.get(key)[-1]))
+        for key, value in SampleA.sample.items():
+            plt.plot(SampleA.stepList, value, label=key + " = {:.2f} mol/L".format(SampleA.sample.get(key)[-1]))
 
         plt.xlabel('Time (t)')
         plt.ylabel('Concentration (mol/L)')
+        plt.suptitle("Equilibrating sample A " + str(SampleA.steps) + " cycles ( " + GetPercent(i, SampleA.steps) + " % done )", fontsize=18)
         plt.legend()
 
-        if(i <= SampleQ.steps):
-            SampleQ.Euler(SampleQ, i)
-            SampleQ.ApplyTitration(SampleQ, i+1)
+        if(i <= SampleA.steps):
+            SampleA.Euler(SampleA, i)
+            SampleA.ApplyTitration(SampleA, i+1)
 
 
-equilibrate(SampleQ, 5.0E-4, 20000)
+class SampleB():
+    sample = {
+        "C":[0],
+        "D":[10]
+    }
+
+    def AccTitration(self, act, titra):
+        titra += self.h
+        if(act <= titra):
+            result = math.floor(titra/act)
+            titra = titra - act * result
+            return result, titra
+        else:
+            return 0, titra
+
+    stepList = []
+    index = count()
+    steps = 100
+    h = 0.0025
+
+    RemMol0=0
+
+    def Euler(self, i) :
+        if(i < self.steps):
+            r0=1*(self.sample.get("C")[-1]**1)
+            r0=2*(self.sample.get("D")[-1]**1)
+            r1=2*(self.sample.get("C")[-1]**2)
+
+            self.sample["C"].append(self.sample.get("C")[-1]+(r0*(-1)+r0*(-1)+r1*(-2))*self.h)
+            self.sample["D"].append(self.sample.get("D")[-1]+(r0*(1)+r0*(1)+r1*(2))*self.h)
+
+    def ApplyTitration(self,i):
+        Result, self.RemMol0 = self.AccTitration(self, 0.3, self.RemMol0)
+        if Result > 0 and self.sample.get("D")[-1]-1 <= 0:
+            self.sample.get("D")[-1] = 0
+        elif Result > 0:
+            self.sample["D"][-1] = self.sample.get("D")[-1]-Result*1
+
+
+    @staticmethod
+    def Animate(i) :
+        plt.cla()
+
+        if(i <= SampleB.steps):
+            SampleB.stepList.append(next(SampleB.index)*SampleB.h)
+
+        for key, value in SampleB.sample.items():
+            plt.plot(SampleB.stepList, value, label=key + " = {:.2f} mol/L".format(SampleB.sample.get(key)[-1]))
+
+        plt.xlabel('Time (t)')
+        plt.ylabel('Concentration (mol/L)')
+        plt.suptitle("Equilibrating sample B " + str(SampleB.steps) + " cycles ( " + GetPercent(i, SampleB.steps) + " % done )", fontsize=18)
+        plt.legend()
+
+        if(i <= SampleB.steps):
+            SampleB.Euler(SampleB, i)
+            SampleB.ApplyTitration(SampleB, i+1)
+
+
+class SampleC():
+    sample = {
+    }
+
+    def AccTitration(self, act, titra):
+        titra += self.h
+        if(act <= titra):
+            result = math.floor(titra/act)
+            titra = titra - act * result
+            return result, titra
+        else:
+            return 0, titra
+
+    stepList = []
+    index = count()
+    steps = 100
+    h = 0.0025
+
+    def Euler(self, i) :
+        pass
+
+    @staticmethod
+    def Animate(i) :
+        plt.cla()
+
+        if(i <= SampleC.steps):
+            SampleC.stepList.append(next(SampleC.index)*SampleC.h)
+
+        for key, value in SampleC.sample.items():
+            plt.plot(SampleC.stepList, value, label=key + " = {:.2f} mol/L".format(SampleC.sample.get(key)[-1]))
+
+        plt.xlabel('Time (t)')
+        plt.ylabel('Concentration (mol/L)')
+        plt.suptitle("Equilibrating sample C " + str(SampleC.steps) + " cycles ( " + GetPercent(i, SampleC.steps) + " % done )", fontsize=18)
+        plt.legend()
+
+        if(i <= SampleC.steps):
+            SampleC.Euler(SampleC, i)
+
+
+
+equilibrate(SampleA, 5.0E-4, 100)
+SampleC.sample = mix([SampleA.sample, SampleB.sample])
+def Euler0(self, i) :
+    if(i < self.steps):
+        r0=1*(self.sample.get("Y")[-1]**1)
+        r1=2*(self.sample.get("X")[-1]**2)
+        r2=1*(self.sample.get("C")[-1]**1)
+        r2=2*(self.sample.get("D")[-1]**1)
+        r3=2*(self.sample.get("C")[-1]**2)
+
+        self.sample["C"].append(self.sample.get("C")[-1]+(r2*(-1)+r2*(-1)+r3*(-2))*self.h)
+        self.sample["D"].append(self.sample.get("D")[-1]+(r2*(1)+r2*(1)+r3*(2))*self.h)
+        self.sample["X"].append(self.sample.get("X")[-1]+(r0*(1)+r1*(-2))*self.h)
+        self.sample["Y"].append(self.sample.get("Y")[-1]+(r0*(-1)+r1*(1))*self.h)
+
+SampleC.Euler = Euler0
+def ApplyTitration0(self,i):
+    if self.sample.get("X")[-1]<3:
+        Result, self.AddMol0 = self.AccTitration(self, 0.1, self.AddMol0)
+        self.sample["X"][-1] = self.sample.get("X")[-1]+Result*1
+    Result, self.RemMol0 = self.AccTitration(self, 0.3, self.RemMol0)
+    if Result > 0 and self.sample.get("D")[-1]-1 <= 0:
+        self.sample.get("D")[-1] = 0
+    elif Result > 0:
+        self.sample["D"][-1] = self.sample.get("D")[-1]-Result*1
+
+SampleC.ApplyTitration = ApplyTitration0
+split(SampleC.sample,[SampleA.sample, SampleB.sample], [0.4, 0.5])
+disposePercent(SampleA.sample,1)
+disposePercent(SampleB.sample,0.5)
